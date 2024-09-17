017b29273abb8aa81d76997cb96ab8b840557a72
Mudassir Ali <deadlool45@gmail.com>
2024-09-17 16:12:33 +0500

    making get api for issue

--- app/api/issues/POST/route.ts
+++ app/api/issues/POST/route.ts
@@ -1,13 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
-});
+import prisma from "../../../lib/prisma"
+import {createIssueSchema} from "../../../lib/validation"
 
 export async function POST(request: NextRequest) {
   try {
