606cfac0eea5fdfb4865f1313172c8fac26e00ac
Mudassir Ali <deadlool45@gmail.com>
2024-09-18 11:29:21 +0500

    navbar basic styling

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -17,15 +17,15 @@ const Navbar = () => {
         }
     ]
   return (
-    <nav className='flex space-x-4 px-6 py-4 border-[1px] items-center'>
-        <Link href="/" className='text-[1.6rem]'><AiFillBug />
+    <nav className='flex space-x-4 px-6 py-4 items-center bg-red-400'>
+        <Link href="/" className='text-[1.6rem] text-white drop-shadow-lg'><AiFillBug />
         </Link>
         <div>
             <ul className='flex gap-4'>
             {links.map(link => <Link key={link.href} href={link.href} className={classnames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-900 transition-color duration-200": true,
+                "text-zinc-100": link.href === currentPath,
+                "text-zinc-300": link.href !== currentPath,
+                "hover:text-zinc-100 transition-color duration-200": true,
             })}>{link.label}</Link>)}
             </ul>
         </div>
